e6601e5aacdc66a1633cacbc585146148348941b
tagawa <tagawa@mahoshi.jp>
2013-09-12 22:37:06 +0900

    Modifiedを修正

--- lib/common.php
+++ lib/common.php
@@ -6,7 +6,7 @@
  * Author : TAGAWA Takao (dounokouno@gmail.com)
  * License : MIT License
  * Since : 2010-11-19
- * Modified : 2013-07-05
+ * Modified : 2013-09-12
 */
 
 // ----------------------------------------------------------------
