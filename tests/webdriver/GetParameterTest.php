9793291f1f14eb16adae6a2ee22f08d0924e3eaf
TAGAWA Takao <dounokouno@gmail.com>
2015-02-18 20:53:42 +0900

    機能テストを追加

---
+++ tests/webdriver/GetParameterTest.php
@@ -0,0 +1,30 @@
+<?php
+/**
+ * Get parameter test.
+ *
+ * @package    TransmitMail
+ * @subpackage PHPUnit with Selenium 2
+ * @author     TAGAWA Takao
+ * @license    MIT License
+ * @copyright  TransmitMail Development Team
+ * @link       https://github.com/dounokouno/TransmitMail
+ */
+
+class GetParameterTest extends TransmitMailFunctionalTest
+{
+
+    /**
+     * GETパラメータの受け取り、出力のテスト
+     */
+    public function testGetParameter()
+    {
+        $getParameter = 'GETパラメータサンプル';
+
+        $this->url('?example=' . urlencode($getParameter));
+        $this->assertEquals($getParameter, $this->byCssSelector('input[type="hidden"][name="GET値取得サンプル"]')->value());
+        $this->inputRequiredField();
+        $this->submitInputForm();
+        $this->assertEquals($this->confirmPageTitle, $this->title());
+        $this->assertContains($getParameter, $this->byCssSelector('#content table')->text());
+    }
+}
