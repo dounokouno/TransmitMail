68914cd92dddca5b338aef398d25e35b0286e202
TAGAWA Takao <dounokouno@gmail.com>
2015-06-20 07:58:29 +0900

    リビジョン番号を修正

--- lib/common.php
+++ lib/common.php
@@ -13,7 +13,7 @@
 // システム名、バージョン
 // ----------------------------------------------------------------
 define('SYSTEM_NAME', 'TransmitMail');
-define('VERSION', '1.5.13');
+define('VERSION', '1.5.14');
 
 // 入力情報として除外する項目
 define('EXCLUSION_ITEM', '[
