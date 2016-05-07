945232914a2f7766e81e9e9c18df12d886156fa9
TAGAWA Takao <dounokouno@gmail.com>
2016-05-07 11:17:40 +0900

    リリースバージョンを修正

--- lib/TransmitMail.php
+++ lib/TransmitMail.php
@@ -12,7 +12,7 @@ class TransmitMail
 {
     // システム情報
     const SYSTEM_NAME = 'TransmitMail';
-    const VERSION = '2.0.7';
+    const VERSION = '2.0.8';
 
     // グローバルエラー
     public $global_errors = array();
