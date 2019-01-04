2c0e3d02de9543f93b74e81aa588143b9c4755e9
TAGAWA Takao <dounokouno@gmail.com>
2019-01-04 22:21:09 +0900

    :up: Bump the version number to 2.2.0

--- lib/TransmitMail.php
+++ lib/TransmitMail.php
@@ -12,7 +12,7 @@ class TransmitMail
 {
     // システム情報
     const SYSTEM_NAME = 'TransmitMail';
-    const VERSION = '2.1.1';
+    const VERSION = '2.2.0';
 
     // グローバルエラー
     public $global_errors = array();
