4129cede10b4f76b7e86f321eefb648afb57571d
Toru Kokubun <kokubun@d-s-b.jp>
2020-02-18 16:02:36 +0900

    テンプレートの出力処理を単独のファンクションとして追加

--- lib/TransmitMail.php
+++ lib/TransmitMail.php
@@ -989,7 +989,7 @@ class TransmitMail
             $this->tpl->set('global_errors', $this->global_errors);
 
             // HTML を表示
-            echo $this->tpl->fetch($this->config['tpl_error']);
+            $this->viewTemplate('tpl_error');
         } elseif ($this->page_name === 'finish') {
             // メール送信
             $this->sendMail();
@@ -1035,18 +1035,18 @@ class TransmitMail
                 $this->tpl->set('global_errors', $this->global_errors);
 
                 // HTML を表示
-                echo $this->tpl->fetch($this->config['tpl_error']);
+                $this->viewTemplate('tpl_error');
             } else {
                 // エラーがない場合
 
                 // 完了画面を表示
-                echo $this->tpl->fetch($this->config['tpl_finish']);
+                $this->viewTemplate('tpl_finish');
             }
         } elseif ($this->page_name === 'confirm') {
             // 確認画面
 
             // HTML を表示
-            echo $this->tpl->fetch($this->config['tpl_confirm']);
+            $this->viewTemplate('tpl_confirm');
         } else {
             // 入力画面 or 入力エラー画面
 
@@ -1059,8 +1059,16 @@ class TransmitMail
             $this->tpl->set('global_errors', $this->global_errors);
 
             // HTMLを表示
-            echo $this->tpl->fetch($this->config['tpl_input']);
+            $this->viewTemplate('tpl_input');
         }
+    }
+
+    /**
+     * テンプレートの出力処理
+     */
+    public function viewTemplate($template_type)
+    {
+        echo $this->tpl->fetch($this->config[$template_type]);
     }
 
     /**
@@ -1599,7 +1607,7 @@ class TransmitMail
 
         // HTMLを表示
         ob_end_clean();
-        echo $this->tpl->fetch($this->config['tpl_error']);
+        $this->viewTemplate('tpl_error');
         exit;
     }
 
