3fd1a839d7ba99b57a423d2a17df2b4bebf2bbea
TAGAWA Takao <dounokouno@gmail.com>
2020-12-28 19:59:25 +0900

    💚 Add PHP 8.0 test

---
+++ tests/bootstrap-php8.php
@@ -0,0 +1,14 @@
+<?php
+/**
+ * PHPUnit bootstrap.
+ *
+ * @package    TransmitMail
+ * @subpackage PHPUnit with Selenium 2
+ * @license    MIT License
+ * @copyright  TransmitMail development team
+ * @link       https://github.com/dounokouno/TransmitMail
+ */
+
+require_once dirname(__FILE__) . '/../vendor/autoload.php';
+require_once dirname(__FILE__) . '/FunctionalTestPhp8/TransmitMailFunctionalTest.php';
+require_once dirname(__FILE__) . '/../lib/TransmitMail.php';
